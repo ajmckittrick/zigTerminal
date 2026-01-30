9788e491175e75f0faaa5b027e157066fad6194d
Drew <149429497+ajmckittrick@users.noreply.github.com>
2026-01-30 15:30:16 +0000

    Added Some code

--- main.zig
+++ main.zig
@@ -1,1 +1,18 @@
+const std = @import("std");
 
+pub fn main() !void {
+
+    //Setup stdout
+    var stdoutWriter = std.fs.File.stdout().writer(&.{});
+    const stdout = &stdoutWriter.interface;
+
+    //Setup stdin
+    var stdinBuff: [1024]u8 = undefined;
+    var stdinReader = std.fs.File.stdin().reader(&stdinBuff);
+    const stdin = &stdinReader.interface;
+
+    _ = stdout;
+    _ = stdin;
+
+    return;
+}
