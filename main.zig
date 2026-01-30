8ada2c558dbb25f9643940489a68623cf6496016
Drew <149429497+ajmckittrick@users.noreply.github.com>
2026-01-30 18:51:29 +0000

    added some more code but can't figure out getting input from argv

--- main.zig
+++ main.zig
@@ -1,6 +1,21 @@
 const std = @import("std");
 
+fn getArgv(alloc: std.mem.Allocator) [*][:0]const u8 {
+    var args = try std.process.argsWithAllocator(alloc);
+    var argsString: [*][:0]const u8 = undefined;
+    while (args.next()) |arg| {
+        argsString[0] = arg;
+    }
+
+    return argsString;
+}
+
 pub fn main() !void {
+
+    //Setup allocator
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = arena.allocator();
+    defer arena.deinit();
 
     //Setup stdout
     var stdoutWriter = std.fs.File.stdout().writer(&.{});
@@ -11,7 +26,10 @@ pub fn main() !void {
     var stdinReader = std.fs.File.stdin().reader(&stdinBuff);
     const stdin = &stdinReader.interface;
 
-    _ = stdout;
+    //Get args
+    const args = getArgv(allocator);
+
+    try stdout.print("I recieved {s}\n", .{args});
     _ = stdin;
 
     return;
